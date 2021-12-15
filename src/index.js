c9d70112db64f58b0527c945eb9b86a44b78d088
starxburst <starburstxstream@google.com>
2021-12-15 08:29:53 -0800

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {testFactory} from './todo-list-factory';
 import {checkLocalStorage, populateStorage} from './storage';
 import createHomePage from './home';
 import {createProjectSidebar} from './project';
-import createTask from './add-task';
+import {createTask} from './add-task';
 
 checkLocalStorage();
 createHomePage();
